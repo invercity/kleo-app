980561e2d0935a2442b9ff44c3054b5ba485e8c1
invercity <andriy.ermolenko@gmail.com>
2015-09-06 22:53:37 +0300

    created article controller

---
+++ public/modules/articles/directives/article-list.client.directive.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('articles').directive('articleList', [
+	function() {
+		return {
+			templateUrl: 'modules/articles/views/templates/article-list.client.html',
+			restrict: 'E',
+			scope: {
+				items: "="
+			},
+			link: function postLink(scope, element, attrs) {
+
+			}
+		};
+	}
+]);
