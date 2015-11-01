82dcadd7b360064b4a795452918c02c50ec7d1a5
invercity <andriy.ermolenko@gmail.com>
2015-11-01 23:47:33 +0200

    updated post configuration

--- public/modules/posts/config/posts.client.config.js
+++ public/modules/posts/config/posts.client.config.js
@@ -4,9 +4,14 @@
 angular.module('posts').run(['Menus', 'AdminView',
 	function(Menus, AdminView) {
 		// Set top bar menu items
+
 		Menus.addMenuItem('topbar', 'Posts', 'posts', 'dropdown', '/posts(/create)?');
 		Menus.addSubMenuItem('topbar', 'posts', 'List Posts', 'posts');
 		Menus.addSubMenuItem('topbar', 'posts', 'New Post', 'posts/create');
+
+		/*
+		Menus.addMenuItem('topbar', 'Posts', 'posts', '/posts');
+		*/
 		AdminView.addAdminPage('posts', 'posts/views/admin.client.view.html');
 	}
 ]);
