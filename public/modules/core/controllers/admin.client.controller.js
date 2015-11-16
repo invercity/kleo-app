5d8dda8d1ba20ba2193c46cc7312a06c44df2d6d
invercity <andriy.ermolenko@gmail.com>
2015-11-16 23:50:16 +0200

    Added logic for selection on admin page

--- public/modules/core/controllers/admin.client.controller.js
+++ public/modules/core/controllers/admin.client.controller.js
@@ -3,5 +3,11 @@
 angular.module('core').controller('AdminController', ['$scope', 'AdminView',
 	function($scope, AdminView) {
 		$scope.pages = AdminView.getAdminPages();
+
+		$scope.selectedPage = $scope.pages.length ? $scope.pages[0] : null;
+
+		$scope.changeSelected = function(page) {
+			$scope.selectedPage = page;
+		}
 	}
 ]);
