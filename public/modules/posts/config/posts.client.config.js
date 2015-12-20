d171822536150b8e218577628a5b43a870bad3f9
invercity <andriy.ermolenko@gmail.com>
2015-12-20 23:51:08 +0200

    Changed post logic

--- public/modules/posts/config/posts.client.config.js
+++ public/modules/posts/config/posts.client.config.js
@@ -5,9 +5,9 @@ angular.module('posts').run(['Menus', 'AdminView', 'Posts',
 	function(Menus, AdminView, Posts) {
 		// Set top bar menu items
 
-		Menus.addMenuItem('topbar', 'Posts', 'posts', 'dropdown', '/posts(/create)?');
+		Menus.addMenuItem('topbar', 'Posts', 'posts'); /*, 'dropdown', '/posts(/create)?');
 		Menus.addSubMenuItem('topbar', 'posts', 'List Posts', 'posts');
-		Menus.addSubMenuItem('topbar', 'posts', 'New Post', 'posts/create');
+		Menus.addSubMenuItem('topbar', 'posts', 'New Post', 'posts/create'); */
 
 		/*
 		Menus.addMenuItem('topbar', 'Posts', 'posts', '/posts');
