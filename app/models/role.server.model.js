a367edebd2725653ef47e26926436cb13d48aebb
invercity <andriy.ermolenko@gmail.com>
2016-01-03 23:48:35 +0200

    Added Role server model

---
+++ app/models/role.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Page Schema
+ */
+var RoleSchema = new Schema({
+  name: String,
+  permissions: Schema.Types.Mixed
+});
+
+mongoose.model('Role', RoleSchema);
