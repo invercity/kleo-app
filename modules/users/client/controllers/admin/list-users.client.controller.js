e7c13866c891c7efcec87d5f15b054143fd569fb
invercity <andriy.ermolenko@gmail.com>
2016-01-16 02:55:08 +0200

    Made admin page skeleton

--- modules/users/client/controllers/admin/list-users.client.controller.js
+++ modules/users/client/controllers/admin/list-users.client.controller.js
@@ -27,5 +27,50 @@ angular.module('users.admin').controller('UserListController', ['$scope', '$filt
     $scope.pageChanged = function () {
       $scope.figureOutItemsToDisplay();
     };
+
+    $scope.data = [
+      {
+        name: 'Models',
+        items: [
+          {name: 'Users'},
+          {name: 'Posts'},
+          {name: 'Roles'}
+        ]
+      }, {
+        name: 'Pages',
+        items: [
+          {name: 'Position'},
+          {name: 'Options'}
+        ]
+      }
+    ];
+
+    $scope.selected = {
+      head: null,
+      subhead: null
+    };
+
+    $scope.setActive = function(item) {
+      if ($scope.selected.subhead && item != $scope.selected.subhead) $scope.selected.subhead.active = false;
+      item.active = true;
+      $scope.selected.subhead = item;
+    };
+
+    $scope.setActiveHead = function(head) {
+      if ($scope.selected.head && head != $scope.selected.head) {
+        $scope.selected.head.active = false;
+        $scope.selected.subhead.active = false;
+      }
+      head.active = true;
+      $scope.selected.head = head;
+      head.items[0].active = true;
+      $scope.selected.subhead = head.items[0];
+    };
+
+    $scope.setActiveHead($scope.data[0]);
+
+    $scope.$watch('selected', function(n, o) {
+      console.log('new: ', n, ', old: ', o)
+    })
   }
 ]);
