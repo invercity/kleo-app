c16f5de369f09ceeeb2edf9fc761bd1cc3181f1f
invercity <andriy.ermolenko@gmail.com>
2016-01-18 00:43:04 +0200

    Removed extra scope data

--- modules/users/client/controllers/settings/settings.client.controller.js
+++ modules/users/client/controllers/settings/settings.client.controller.js
@@ -3,16 +3,5 @@
 angular.module('users').controller('SettingsController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     $scope.user = Authentication.user;
-    $scope.tabs = [
-      {
-        id: 'user',
-        title: 'User settings',
-        items: [
-          {
-
-          }
-        ]
-      }
-    ]
   }
 ]);
