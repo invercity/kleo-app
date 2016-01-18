f43392744f0ee958bead7692723115cad1479fe5
invercity <andriy.ermolenko@gmail.com>
2016-01-19 01:14:15 +0200

    Changed policies for post module

--- modules/posts/server/policies/posts.server.policy.js
+++ modules/posts/server/policies/posts.server.policy.js
@@ -49,7 +49,7 @@ exports.isAllowed = function (req, res, next) {
   var roles = (req.user) ? req.user.roles : ['guest'];
 
   // If an article is being processed and the current user created it then allow any manipulation
-  if (req.post && req.user && req.post.user.id === req.user.id) {
+  if ((roles.indexOf('admin') != -1) || req.post && req.user && req.post.user.id === req.user.id) {
     return next();
   }
 
