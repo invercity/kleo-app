f9087c5b9494fa7ac6b7f75e18c39133fa3e8b39
invercity <andriy.ermolenko@gmail.com>
2016-02-15 23:48:43 +0200

    Removed some static data

--- modules/core/client/controllers/footer.client.controller.js
+++ modules/core/client/controllers/footer.client.controller.js
@@ -3,5 +3,12 @@
 angular.module('core').controller('FooterController', ['$scope', 'SystemData', function($scope, SystemData) {
   SystemData.getData().then(function(data) {
     $scope.app = data;
+
+    // temporary data
+    $scope.links = _.map(_.range(0, 4), function(el) {
+      return _.map(_.range(0, 4), function(row) {
+        return 'Map Site Link #' + (el * 4 + row + 1);
+      });
+    });
   });
 }]);
