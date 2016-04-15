e9a8fa4e18b70db49723eb6ad8555b0b12f192a2
invercity <andriy.ermolenko@gmail.com>
2016-04-15 22:43:17 +0300

    Important security fix

--- test.js
+++ test.js
@@ -8,5 +8,5 @@ var path = require('path');
 var app = require(path.resolve('./config/lib/app'));
 
 app.init(function () {
-  console.log('Initialized test automation');
+  console.log('Initialized test automation.');
 });
